170a374d5dc9e29f6956cb1d3642a661758a2223
Raquel Brito <raquelbrito662@gmail.com>
2024-04-05 22:03:50 -0300

    init

---
+++ src/main/java/com/pro/gymfit/GymfitApplication.java
@@ -0,0 +1,13 @@
+package com.pro.gymfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/gymfit/GymfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.gymfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
